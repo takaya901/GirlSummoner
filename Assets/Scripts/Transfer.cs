55f30703801f708edda5cfeb3ad765da792702c0
Takaya Ogawa <bumpofchicken344@gmail.com>
2020-04-15 00:36:17 +0900

    add transfer shader

---
+++ Assets/Scripts/Transfer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transfer : MonoBehaviour
+{
+    Material _transferMat;
+    string _threshold = "_Threshold";
+    float _currentVelocity;
+    
+    void Start()
+    {
+        _transferMat = GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        var height = Mathf.SmoothDamp(_transferMat.GetFloat(_threshold), 1f, ref _currentVelocity, 2f);
+        if (_transferMat.HasProperty(_threshold)) {
+            _transferMat.SetFloat(_threshold, height);
+        }
+    }
+}
